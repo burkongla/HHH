fda03640ac1bc9ed9d795936e2e3968e469baa41
burkongla <stianmichael@gmail.com>
2015-05-05 09:36:55 +0200

    Working like a charm.

--- src/no/ntnu/ttm4115/hhh/heater/component/Component.java
+++ src/no/ntnu/ttm4115/hhh/heater/component/Component.java
@@ -10,30 +10,75 @@ public class Component extends Block {
 
 	public float DesiredTemperature;
 	public float CurrentTemperature;
+	public boolean Status;
+	public java.lang.String looPayload;
 
 	public Parameters MQTTSetup() {
 		MQTTConfigParam m = new MQTTConfigParam("dev.bitreactive.com");
-		m.addSubscribeTopic("hhh/heaters/heater1");
+		m.addSubscribeTopic("hhh/heaters");
 		Parameters p = new Parameters(m);
 		return p;
 	}
 
 	public void msgHandler(MQTTMessage m) {
+		System.out.println("Entered msgHandler.");
 		String[] message = new String(m.getPayload()).split("\\s+");
-		if (message[0].equals("Current:")) {
-			this.CurrentTemperature = Float.valueOf(message[1]); 
-		} else if (message[0].equals("Desired:")) {
-			this.DesiredTemperature = Float.valueOf(message[1]);
+		
+		if(message[0].equals("Heating:")) {
+			setStatus(true);
+			setLooPayload(message[0] + " " + message[1]);
+			
+		} else if (message[0].equals("Cooling:")) {
+			setStatus(true);
+			setLooPayload(message[0] + " " + message[1]);
+		} else if (message[0].equals("Off:")) {
+			setStatus(false);
 		}
 	}
 
 	public MQTTMessage createMessage() {
-		String payload = "Current:"+" "+String.valueOf(CurrentTemperature);
+		System.out.println("Sending message");
+		String payload = getLooPayload();
 		byte[] bytes = payload.getBytes();
-	    String topic = "hhh/heaters";
+	    String topic = "hhh/thermostats";
+	    System.out.println(payload);
 		MQTTMessage message = new MQTTMessage(bytes, topic);
 		message.setQoS(2);
 		return message;
 	}
+
+	public float getDesiredTemperature() {
+		return DesiredTemperature;
+	}
+
+	public void setDesiredTemperature(float desiredTemperature) {
+		DesiredTemperature = desiredTemperature;
+	}
+
+	public float getCurrentTemperature() {
+		return CurrentTemperature;
+	}
+
+	public void setCurrentTemperature(float currentTemperature) {
+		CurrentTemperature = currentTemperature;
+	}
+
+	public boolean isStatus() {
+		return Status;
+	}
+
+	public void setStatus(boolean status) {
+		Status = status;
+	}
+
+	public java.lang.String getLooPayload() {
+		return looPayload;
+	}
+
+	public void setLooPayload(java.lang.String looPayload) {
+		this.looPayload = looPayload;
+	}
+	
+	
 
 }
